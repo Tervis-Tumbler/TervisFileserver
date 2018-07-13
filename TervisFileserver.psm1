5b68976dced9f289fb4afec66a8d03de477de56d
ChrisMagnuson <hourglasssand@gmail.com>
2018-07-13 11:48:31 -0400

    Remove Get-PasswordstateCredential

--- TervisFileserver.psm1
+++ TervisFileserver.psm1
@@ -124,7 +124,7 @@ function Install-ExplorerFavoritesScheduledTasks {
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
     )
     begin {
-        $ScheduledTaskCredential = New-Object System.Management.Automation.PSCredential (Get-PasswordstateCredential -PasswordID 259)
+        $ScheduledTaskCredential = New-Object System.Management.Automation.PSCredential (Get-PasswordstatePassword -AsCredential -ID 259)
         $Execute = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
         $Argument = '-NoProfile -Command Push-TervisExplorerFavoritesOrQuickAccess -ComputerOrganizationalUnit "OU=Departments,DC=tervis,DC=prv" > c:\schedoutput\PushExplorerFavorites.log'
     }
