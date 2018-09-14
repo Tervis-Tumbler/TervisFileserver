c9c16175b4990e7746c9713ad66e41ceb546459c
Don Mohlmaster <mastermohl@gmail.com>
2018-09-14 15:37:47 -0400

    Var fixes

--- TervisFileserver.psm1
+++ TervisFileserver.psm1
@@ -515,9 +515,9 @@ p-odbee02
 p-weblogic01
 p-weblogic02
 p-infadac
-"@ -split "`r`n"
+"@ -split "`n"
     $PasswordstateCredential = New-Object System.Management.Automation.PSCredential (Get-PasswordstatePassword -AsCredential -ID 5574)
-    ForEach($ComputerName in $LocalLinuxPathDefinition){
+    ForEach($ComputerName in $LinuxServersToMonitor){
         if(-not (Get-SSHSession -ComputerName $ComputerName)){
             $SSHSession = New-SSHSession -ComputerName $ComputerName $PasswordstateCredential -AcceptKey 
         }
